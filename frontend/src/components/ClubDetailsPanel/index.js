182b016b26b37b6ec171880755d1dabc212aca9f
playerzero <abingigo@gmail.com>
2021-12-01 11:17:46 +0530

    integrated backend-Abin

--- frontend/src/components/ClubDetailsPanel/index.js
+++ frontend/src/components/ClubDetailsPanel/index.js
@@ -1,38 +1,48 @@
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import {useEffect, useState } from "react";
 import expandIcon from "../../icons/expand.svg";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ClubDetailsPanel = () => {
+    const [clubInfo, setClubInfo] = useState(null);
+    const userToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzODI4MjE3NSwianRpIjoiYjgyNDU3N2EtOGJiZi00ODczLTk2MWMtNTY3ODI0NWU3ZTU5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IkNhcGl0YWxpc3RzIiwibmJmIjoxNjM4MjgyMTc1LCJleHAiOjE2MzgzNjg1NzV9.h0LOx7E7ZiupPkpsdCPKfBQUznsYv5Qos8n9uDL9bek";
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if (!userToken) {
+        navigate("/");
+      }
+      axios
+        .get(process.env.REACT_APP_BACKEND_URL + "club_info", {
+          headers: { Authorization: `Bearer ${userToken}` },
+        })
+        .then((res) => {
+          setClubInfo(res.data.info);
+        });
+    });
+    
     return (
       <>
         <div className="p-4 text-white" style={{height:"100vh"}}>
       <h1>
-        <b>{"Insert club name here"}</b>
+        <b>{clubInfo?.club_name}</b>
       </h1>
-      <p className="form-control bg-transparent text-white">Club description here</p>
+      <p className="form-control bg-transparent text-white">{clubInfo?.club_desc}</p>
       <br />
       <br />
       <div className="collapse-cards" style={{height: "80vh", display: "flex"}}>
         <div>
           <h3>Members</h3>
           <div className="overflow-auto" style={{ height: "500px", width: "700px"}}>
-            {[
-              "Person A",
-              "Person B",
-              "Person C",
-              "Person B",
-              "Person C",
-              "Person B",
-              "Person C",
-              // "Person B",
-              // "Person C",
-              "Person B",
-              // "Person C",
-            ].map((name) => {
+          {clubInfo?.members.map(({ name, roll_no, position }) => {
               return (
                 <Card className="w-100 mb-2 text-black">
                   <Card.Body>
                     <Card.Title>{name}</Card.Title>
+                    <Card.Text className="float-start">
+                      {position}
+                    </Card.Text>
                   </Card.Body>
                 </Card>
               );
@@ -42,17 +52,23 @@ const ClubDetailsPanel = () => {
           <div>
           <h3>Events</h3>
           <div className="overflow-auto" style={{ height: "500px", width:"700px"}}>
-            {["Event A", "Event B"].map((name) => {
+          {clubInfo?.events.map(({ event_name, date, slot, event_id }) => {
               return (
                 <Card className="w-100 mb-2 text-black">
                   <Card.Body>
-                    <Card.Title>{name}</Card.Title>
+                    <Card.Title>{event_name}</Card.Title>
                     <Card.Text className="float-start m-0">
-                      ECLC
+                      {date.substring(0, date.indexOf("00:00:00"))}
                       <br />
-                      Friday 10:30pm
+                      {slot}
                     </Card.Text>
-                    <input type="image" src={expandIcon} className="float-end" style={{ height: "20px" }} />
+                    <input
+                      type="image"
+                      src={expandIcon}
+                      className="float-end"
+                      style={{ height: "20px" }}
+                      onClick={() => navigate(`${event_id}`)}
+                    />
                   </Card.Body>
                 </Card>
               );
--- frontend/src/components/SysVenuePanel/MakeVenue.js
+++ frontend/src/components/SysVenuePanel/MakeVenue.js
@@ -1,18 +1,31 @@
 import Modal from "react-bootstrap/Modal";
-import Spinner from "react-bootstrap/Spinner";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useCallback, useContext, useState } from "react";
-import { AppContext } from "../../App";
 
 const VenueAddModal = () => {
+
   const [name, setName] = useState("");
 
-  const { setUserToken } = useContext(AppContext);
+  const userToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzODI4MjE3NSwianRpIjoiYjgyNDU3N2EtOGJiZi00ODczLTk2MWMtNTY3ODI0NWU3ZTU5IiwidHlwZSI6ImFjY2VzcyIsInN1YiI6IkNhcGl0YWxpc3RzIiwibmJmIjoxNjM4MjgyMTc1LCJleHAiOjE2MzgzNjg1NzV9.h0LOx7E7ZiupPkpsdCPKfBQUznsYv5Qos8n9uDL9bek";
 
   const handleCreate = useCallback(() => {
-    
-  }, [name, setUserToken]);
+    (e) => {
+      e.preventDefault();
+      axios
+        .post(
+          process.env.REACT_APP_BACKEND_URL + "venue_add",
+          {
+            venue_name: name,
+          },
+          { headers: { Authorization: `Bearer ${userToken}` } }
+        )
+        .then(() => {
+          setName({ ...name, name: null });
+        });
+    },
+    [name]
+  });
 
   return (
     <Modal.Dialog size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
